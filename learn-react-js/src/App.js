dea3448b410301fb923a1a2f7c55f8f44a0a1870
ghifaryadnann <ghifaryadnann@gmail.com>
2023-12-01 19:46:54 +0700

    First ReactJs Install

---
+++ learn-react-js/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='wrapper'>
+        <h1>Hello World</h1>
+        <hr/>
+        <p>Saya Sudah berhasil install react JS</p>
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
