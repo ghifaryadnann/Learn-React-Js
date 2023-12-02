a244c82ef529e84e1b73f6291c9546e8beb2c0b6
ghifaryadnann <ghifaryadnann@gmail.com>
2023-12-02 14:02:35 +0700

    tailwind first

--- learn-react-js/src/App.js
+++ learn-react-js/src/App.js
@@ -3,15 +3,21 @@ import Belajarhuk from './belajarHuk/Belajarhuk';
 import KomponenPertama from './componentFirst/KomponenPertama';
 import ReactPertama from './firstReactjs/ReactPertama';
 import React from 'react';
+import {Alert} from 'flowbite-react'
 
 
 
 function App() {
   return (
     <>
+    <Alert color="info">
+      <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
+    </Alert>
       <ReactPertama/>
       <KomponenPertama name="ghifary" age="79" hobi="Coding"/>
       <Belajarhuk/>
+
+      
     </>
    
   );
--- learn-react-js/src/belajarHuk/Belajarhuk.jsx
+++ learn-react-js/src/belajarHuk/Belajarhuk.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react'
+import { Button } from 'flowbite-react'
 
 const Belajarhuk = () => {
     const [count, setCount] = useState(0)
-
+const nambahNilai = () => setCount (count + 1)
+    const kurangNilai = () => setCount ( count - 1)
+    const resetNilai = () => setCount (0)
   return (
     <div className='container'>
         <div className='wrapper'>
         <div>
-            <p>{count >= 11 ? <span>Lebih dari sepuluh</span> : <span>{count}</span>}</p>
-            
-            <button onClick={() => setCount(count + 1)}>
-
-            Click me
-            </button>
+            <p>{count >= 11  ? <span>Lebih dari sepuluh</span> : <span>{count}</span>}</p>
+            <Button color="success" onClick={nambahNilai}>Success</Button>
+            <Button color="gray" onClick={kurangNilai}>Kurang</Button>
+            <Button color="success" onClick={resetNilai}>Reset</Button>
         </div>
+        
         </div>
     </div>
   )
